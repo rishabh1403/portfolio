57f9eca1b9fc3ea6a5898d1190160704259cc2c0
Rishabh Jain <mailrishabh1403@gmail.com>
2019-01-29 20:53:26 +0530

    add matrix in background

---
+++ precache-manifest.564b7c92bd1cefdee1d07246f71bae69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d172dc0f6906b113eaac72aa581d7af8",
+    "url": "/static/media/x.d172dc0f.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb2294fe632f142016ae",
+    "url": "/static/js/main.cb2294fe.chunk.js"
+  },
+  {
+    "revision": "d47b5599c9d0b531e861",
+    "url": "/static/js/1.d47b5599.chunk.js"
+  },
+  {
+    "revision": "cb2294fe632f142016ae",
+    "url": "/static/css/main.089acba9.chunk.css"
+  },
+  {
+    "revision": "c68f926a714db6c99208efc8e6a39be5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96d914c9067288b4edcdacc1de88548f.js"
+  "/precache-manifest.564b7c92bd1cefdee1d07246f71bae69.js"
 );
 
 workbox.clientsClaim();
